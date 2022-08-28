aaf874f7c165cf75191dbcf7ed4be9ba624c8201
fadikhalaili <44548050+fadikhalaili@users.noreply.github.com>
2022-08-28 14:49:38 +0200

    Add files via upload

---
+++ Collections/Ex01/Solution.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleUtilities;
+
+namespace CSharpCourse.Collections.Ex01
+{
+    public class Solution
+    {
+        static readonly ConsoleCompanion _cc = new();
+
+        public static void NamesWithListManipulate()
+        {
+            var list = new List<string>();
+
+            while (true)
+            {
+                string name = _cc.AskForString("Enter a name:");
+                if (name.ToLower() == "quit")
+                {
+                    list.Sort();
+
+                    RemoveFirstAndLastValue(list);
+
+                    DisplayPeople(list);
+                    return;
+                }
+                list.Add(name);
+            }
+        }
+
+        private static void RemoveFirstAndLastValue(List<string> list)
+        {
+            if (list.Count == 1)
+                list.RemoveAt(0);
+
+            else if (list.Count >= 2)
+            {
+                list.RemoveAt(0);
+                list.RemoveAt(list.Count - 1);
+            }
+        }
+
+        private static void DisplayPeople(List<string> list)
+        {
+            foreach (var name in list)
+            {
+                _cc.WriteLineDark($"Name: {name}");
+            }
+        }
+    }
+}
---
+++ Collections/Ex02/Solution.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleUtilities;
+
+namespace CSharpCourse.Collections.Ex02
+{
+    public class Solution
+    {
+        static readonly ConsoleCompanion _cc = new();
+
+        public static void ProductDictionary()
+        {
+            Dictionary<int, string> products = BuildProductDictionary();
+            DisplayProductDictionary(products);
+        }
+
+        private static Dictionary<int, string> BuildProductDictionary()
+        {
+            var productDic = new Dictionary<int, string>();
+
+            while (true)
+            {
+                string answer = _cc.AskForString("Enter product id and name:");
+                answer = answer.Trim();
+
+                if (answer == "")
+                    return productDic;
+
+                int productId = int.Parse(answer.Split(',')[0]);
+                string productName = answer.Split(',')[1];
+
+                if (productDic.ContainsKey(productId))
+                    _cc.WriteLineRed($"The product list already contains the id {productId}");
+                else
+                    productDic.Add(productId, productName);
+            }
+        }
+
+        private static void DisplayProductDictionary(Dictionary<int, string> products)
+        {
+            foreach (var product in products)
+            {
+                _cc.WriteLineDark($"Product id={product.Key} and name={product.Value}");
+            }
+        }
+    }
+}
