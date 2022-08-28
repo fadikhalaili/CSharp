2470903d4cb2f2c6d08bcc255132d4920af75997
fadikhalaili <44548050+fadikhalaili@users.noreply.github.com>
2022-08-28 14:53:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿namespace CSharpCourse
+{
+    public class Program
+    {
+        static void Main()
+        {
+            /* 
+
+                Bra kommandon att känna till:
+
+                Add new file        Shift-F2
+                Comment             Ctrl K C
+                Uncomment           Ctrl K U
+                Magic               Ctrl .    (t.ex "Remove usings", "Generate method", "Invert if")
+
+                Gå in i metoden     F12
+                Var används met?    Shift F12
+                Backa               Ctrl -
+
+                Starta m debug      F5
+                Fortsätt debug      F5
+                Starta utan debug   Ctrl-F5
+                Breakpoint          F9
+                Stega               F10
+                Stega in i          F11
+                Avsluta debug       Shift-F5
+
+                Flytta kod          Alt + piltangent
+
+                Intellisence compl. TAB
+
+                Bygga projektet     Shift-Ctrl-B
+             */
+
+            //Fundamentals.Demo.Demo1.Run();
+            //Fundamentals.Demo.Demo2.Run();
+            //Fundamentals.Demo.Demo3.Run();
+
+            //Fundamentals.Ex01.Solution.WhatsYourName();
+            //Fundamentals.Ex02.Solution.WhatsYourName_WithTypes();
+            //Fundamentals.Ex03.Solution.FormatStringsInDiffentWays();
+
+            //Control.Demo.Demo1.Run();
+
+            //Collections.Demo.Demo1.Run();
+            //Collections.Demo.Demo2.Run();
+
+            //Collections.Ex01.Solution.NamesWithListManipulate();
+            //Collections.Ex02.Solution.ProductDictionary();
+            //Collections.Ex03.Solution.ProductDictionaryWithValidation();
+
+            //Linq.Demo.Demo1.Run();
+
+            //Linq.Ex01.Solution_Start.Run();
+            //Linq.Ex01.Solution_Compact.Run();
+            //Exceptions.Demo.Demo0.Run();
+
+            AbstractClasses.Ex01.Solution.Run();
+        }
+    }
+}
