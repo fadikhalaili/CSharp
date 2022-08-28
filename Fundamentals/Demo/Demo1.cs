55c69f7181414b657cd74f448a109edb1a90a697
fadikhalaili <44548050+fadikhalaili@users.noreply.github.com>
2022-08-28 14:51:01 +0200

    Add files via upload

---
+++ Fundamentals/Demo/Demo1.cs
@@ -0,0 +1,17 @@
+﻿// Ctrl . ==> Generell kodhjälp, i detta fallet "Remove unnecessary usings"
+
+namespace CSharpCourse.Fundamentals.Demo
+{
+    public class Demo1
+    {
+        public static void Run()
+        {
+            Console.Write("Enter you height: ");
+            string? answer = Console.ReadLine()!;
+
+            // cw + TAB ==> Console.Writeline
+
+            Console.WriteLine("You are " + answer +"cm long");
+        }
+    }
+}
---
+++ Fundamentals/Demo/Demo3.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharpCourse.Fundamentals.Demo
+{
+    public class Demo3
+    {
+        // Formattera dokumentet: Ctrl K Ctrl D
+        public static void Run()
+        {
+            Console.Write("Enter your height: ");
+            string answer = Console.ReadLine()!;
+
+            decimal height = decimal.Parse(answer);
+
+            if (height < 0)
+                Console.WriteLine("You can't have negative height");
+            else if (height > 200)
+                Console.WriteLine("You are higher than 2m");
+            else
+                Console.WriteLine("You are lower than 2m");
+        }
+    }
+}
